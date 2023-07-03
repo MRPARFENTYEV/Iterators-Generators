dd8e5b537ed5b1f0cf9da2028eb02baf8126edf5
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-07-03 18:03:41 +0300

    rework again

--- FlatIterator.py
+++ FlatIterator.py
@@ -2,34 +2,43 @@
 который принимает список списков и возвращает их плоское представление.
 Функция test в коде ниже также должна отработать без ошибок.'''
 import types
+class FlatIterator:
 
+    def __init__(self, list_of_list):
+        self.items = sum(list_of_list, [])
+        self.items_counter = 0
 
-def flat_generator(list_of_lists):
-    items = sum(list_of_lists, [])
-    items_counter = 0
+    def __iter__(self):
+        return self
 
-    while items_counter < len(items):
-        yield items[items_counter]
-        items_counter += 1
+    def __next__(self):
+        if self.items_counter != len(self.items):
+            item = self.items[self.items_counter]
+            # print(self.items[self.items_counter])
+            self.items_counter += 1
+
+            return item
+        else:
+            raise StopIteration
 
 
 def func():
+
     list_of_lists_1 = [
         ['a', 'b', 'c'],
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
 
+
     for flat_iterator_item, check_item in zip(
-            flat_generator(list_of_lists_1),
+            FlatIterator(list_of_lists_1),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
     ):
-        print(check_item)
+
         assert flat_iterator_item == check_item
 
-    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-
-    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
 
 if __name__ == '__main__':
